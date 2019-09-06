eb619f6079336304e593514b0eab66159670d5a3
Jeff <jefftully@gmail.com>
2019-09-06 11:01:41 -0400

    added storybook addon + test

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -9,3 +9,16 @@ export const task = {
   state: "TASK_INBOX",
   updatedAt: new Date(2018, 0, 1, 9, 0)
 };
+
+export const actions = {
+  onPinTask: action("onPinTask"),
+  onArchiveTask: action("onArchiveTask")
+};
+storiesOf("Task", module)
+  .add("default", () => <Task task={task} {...actions} />)
+  .add("pinned", () => (
+    <Task task={{ ...task, state: "TASK_PINNED" }} {...actions} />
+  ))
+  .add("archived", () => (
+    <Task task={{ ...task, state: "TASK_ARCHIVED" }} {...actions} />
+  ));
